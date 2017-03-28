34f13d4b9bd8f1159118e21846928fe4643ec0da
daj513 <dariusjenkin@gmail.com>
2017-03-28 11:31:12 -0400

    Tinder Card UI

--- app/src/main/java/com/example/phil/httppost/data/remote/ApiUtils.java
+++ app/src/main/java/com/example/phil/httppost/data/remote/ApiUtils.java
@@ -4,8 +4,8 @@ package com.example.phil.httppost.data.remote;
 public class ApiUtils {
     private ApiUtils() {}
 
-    public static final String BASE_URL = "http://10.0.2.2:4000/api/";
-    //public static final String BASE_URL = "http://ubuntu@sepract1.monmouth.edu:4000/api/";
+    public static final String BASE_URL = "http://ec2-34-207-144-227.compute-1.amazonaws.com:4000/api/";
+
 
     public static GoodJobService getAPIService() {
 
