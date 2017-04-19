2cc6fdd6c0e417369c1e32b219460edcca3e4a26
Phil <plantlife26@gmail.com>
2017-04-19 13:17:14 -0400

    chat fixes

--- app/src/main/java/com/example/phil/httppost/ChatActivity.java
+++ app/src/main/java/com/example/phil/httppost/ChatActivity.java
@@ -53,7 +53,7 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_chat);
+        //setContentView(R.layout.activity_chat);
         setContentView(R.layout.chat_updated);
         goodJobService = ApiUtils.getAPIService();
 
